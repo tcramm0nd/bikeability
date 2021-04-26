867455553488cd4f06390f170a68fb3ce3e864f2
tcramm0nd <25987791+tcramm0nd@users.noreply.github.com>
2021-04-25 22:02:01 -0400

    cleaned up overpass api

--- bikeability/overpass.py
+++ bikeability/overpass.py
@@ -66,23 +66,6 @@ class API():
             
         return data
             
-    # def _query_builder(self, search_terms, area_query=None, area_id=None, city=None, admin_level=None):
-
-    #     output = f"""[out:"{self.output}"];"""
-    #     if area_query:
-    #         area = area_query
-    #     elif area_id:
-    #         area_id = 3600000000 + area_id
-    #         area = f'''area({area_id})->.a;'''
-    #     elif city and admin_level:
-    #         area = f'''area["name"="{city}"]["admin_level"="{admin_level}"]->.a;'''
-    #     elif city and not admin_level:
-    #         area = f'''area["name"="{city}"]->.a;'''
-    #     else:
-    #         raise Exception('No search bounding box supplied')
-    #     verbosity = f'''out {self. verbosity};'''
-    #     return output + area + search_terms + verbosity
-            
     def _query_builder(self, search_terms, area_id):
 
         output = f"""[out:"{self.output}"];"""
@@ -106,39 +89,3 @@ def check_bounds(min_lat, min_lon, max_lat, max_lon):
     print(d)
     if  d > 100:
         print('City is incorrect, please provide more details')
-
-
-
-
-most_populous_cities = {'New York': 175905,
-                        'Los Angeles': 207359,
-                        'Chicago': 122604,
-                        'Houston': 2688911,
-                        'Phoenix': 111257,
-                        'Philadelphia': 188022,
-                        'San Antonio': 253556,
-                        'San Diego': 253832,
-                        'Dalls': 6571629,
-                        'San Jose': 112143
-                        }
-
-top_cycling_cities = {'Seattle':237385,
-                      'San Francisco': 111968,
-                      'Fort Collins': 112524,
-                      'Minneapolis': 136712,
-                      'Portland': 186579,
-                      'Chicago': 122604,
-                      'Eugene': 186706,
-                      'Madison': 3352040,
-                      'New York': 175905,
-                      'Cambridge': 1933745
-                      }
-
-cycle_query = """(way[highway=cycleway](area.a);way[cycleway=lane](area.a);
-                   way[cycleway=asl](area.a);way[cycleway=bike_box](area.a);
-                   way[cycleway=crossing](area.a);way[cycleway=opposite_lane](area.a);
-                   way[cycleway=opposite_track](area.a);way[cycleway=separate](area.a);
-                   way[cycleway=sidepath](area.a);way[cycleway=track](area.a);
-                   way[bicycle=designated](area.a);way[bicycle=yes](area.a);
-                   way[bicycle_road=yes](area.a);
-                   );"""
